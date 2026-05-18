c128f737c658784dc75c971d9651471ca9758b62
Sreeja Pagireddy <sreeja@karibu.ai>
2026-05-18 16:05:47 -0700

    first

---
+++ temperture_analysis.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+from scipy.cluster.hierarchy import dendrogram, linkage
+
+#cleaning and reading data
+df = pd.read_csv('top100cities_weather_data.csv')
+#remove duplicates and drop null values
+df.drop_duplicates(inplace = True)
+#drop null values
+df.dropna(inplace=True)
+
+# I want to only check all the columns that are releated to temperature
+df_subset = df.loc[:, ['Temperature (Celsius)', 'Wind Speed (m/s)']]
+
+#distance measure[]
+distance_measure = linkage(df_subset, method='ward', metric='euclidean')
+
+#Title of the plot
+plt.title('Hierarchical Dendogram')
+plt.xlabel('Index')
+plt.ylabel("Distance")
+
+#Dendogram
+dendrogram(distance_measure, labels = df['City'], leaf_rotation= 90)
+
+plt.show()
