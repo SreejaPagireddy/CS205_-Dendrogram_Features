9015f53d6fc2004128253ceea5f0d20e30ff21f5
Sreeja Pagireddy <sreeja@karibu.ai>
2026-06-01 20:32:05 -0700

    added norm"
    d
    "

--- temperture_analysis.py
+++ temperture_analysis.py
@@ -2,6 +2,7 @@ import pandas as pd
 from matplotlib import pyplot as plt
 from scipy.cluster.hierarchy import dendrogram, linkage
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
+import numpy as np
 
 #cleaning and reading data
 df = pd.read_csv('top100cities_weather_data.csv')
@@ -12,13 +13,18 @@ df.dropna(inplace=True)
 
 #only keep number ones
 df.select_dtypes(include='number')
-
-
 # I want to only check all the columns that are releated to temperature
 df_subset = df.loc[:, ['Temperature (Celsius)', 'Wind Speed (m/s)']]
 
+
+#normalization
+encoder = OneHotEncoder(sparse_output=False)
+X_enc = encoder.fit_transform(df_subset)
+sc = StandardScaler()
+x_sc = sc.fit_transform(df_subset)
+X = np.hstack([X_enc, x_sc])
 #distance measure[]
-distance_measure = linkage(df_subset, method='ward', metric='euclidean')
+distance_measure = linkage(X, method='ward', metric='euclidean')
 
 #Title of the plot
 plt.title('Hierarchical Dendogram')
